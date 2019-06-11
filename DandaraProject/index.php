81826f5b6ca618b470f243f1d3269c489f30877b
romarioL <45340806+romarioL@users.noreply.github.com>
2019-06-10 22:30:42 -0300

    Add files via upload

---
+++ DandaraProject/index.php
@@ -0,0 +1,34 @@
+<!doctype html5>
+<html lang="pt">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+
+
+    <title>Projeto Dandara</title>
+  </head>
+  <body>
+
+    <div class="banner container" id="banner">
+        <h1 class="banner-title">Conheça o projeto Dandara</h1>
+        <p> O lugar onde sua voz  jamais será silenciada.</p>
+        <div class="banner-area  row container ">
+          <div class="banner-text col-sm-12 col-lg-5" > Nos propomos a ser o maior projeto open source de coleta de dados e estatísticas de casos de homofobia  no brasil.</div>
+          <div class="banner-text col-sm-12 col-lg-5" > Acreditamos que   as estatísticas  oficiais subrepresentam os números reais, devido  a dependência do que se torna midiático. </div>
+      </div>
+      <a href="<?php echo DIRPAGE ?>">
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/jquery-3.4.1.min.js" ></script>
+    <script src="js/popper.min.js" ></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
